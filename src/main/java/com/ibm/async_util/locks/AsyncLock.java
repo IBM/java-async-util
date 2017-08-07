8b35f670e779a95bfc34657347fd17f90e967733
Ravi Khadiwala <rkhadiwa@us.ibm.com>
2017-08-06 20:31:04 -0500

    Update headers

--- src/main/java/com/ibm/async_util/locks/AsyncLock.java
+++ src/main/java/com/ibm/async_util/locks/AsyncLock.java
@@ -1,3 +1,9 @@
+/*
+* Copyright (c) IBM Corporation 2017. All Rights Reserved.
+* Project name: java-async-util
+* This project is licensed under the Apache License 2.0, see LICENSE.
+*/
+
 package com.ibm.async_util.locks;
 
 import java.util.Optional;
--- src/test/java/com/ibm/async_util/util/Reference.java
+++ src/test/java/com/ibm/async_util/util/Reference.java
@@ -1,21 +1,8 @@
-//
-// (C) Copyright IBM Corp. 2005 All Rights Reserved.
-//
-// Contact Information:
-//
-// IBM Corporation
-// Legal Department
-// 222 South Riverside Plaza
-// Suite 1700
-// Chicago, IL 60606, USA
-//
-// END-OF-HEADER
-//
-// -----------------------
-// @author: rkhadiwala
-//
-// Date: Jul 9, 2016
-// ---------------------
+/*
+* Copyright (c) IBM Corporation 2017. All Rights Reserved.
+* Project name: java-async-util
+* This project is licensed under the Apache License 2.0, see LICENSE.
+*/
 
 package com.ibm.async_util.util;
 
