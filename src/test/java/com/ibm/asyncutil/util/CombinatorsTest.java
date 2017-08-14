eb87343c245dfe80c54b0251a9ed96e4b13c96bb
Renar Narubin <rnarubin@cleversafe.com>
2017-08-14 11:24:15 -0500

    Generalize to CompletableStage in CombinatorsTest
    This is a test where it's important to treat stage implementations
    agnostically, combinators shouldn't rely on CompletableFuture behavior
    such as recursion prevention

--- src/test/java/com/ibm/asyncutil/util/CombinatorsTest.java
+++ src/test/java/com/ibm/asyncutil/util/CombinatorsTest.java
@@ -11,56 +11,90 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
+import com.ibm.async_util.util.TestUtil.CompletableStage;
+
+@RunWith(Parameterized.class)
 public class CombinatorsTest {
   private static class TestException extends RuntimeException {
     private static final long serialVersionUID = 1L;
   }
 
-  @Test
+  @Parameters(name = "{index}: stage impls <{0}>, <{1}>")
+  public static Collection<Object[]> stageFactories() {
+    return TestUtil.stageFactories()
+        .flatMap(completableSupplier -> TestUtil.doneStageFactories()
+            .map(completedFunction -> new Object[] {completableSupplier, completedFunction}))
+        .collect(Collectors.toList());
+  }
+
+  @Parameter(0)
+  public Supplier<CompletableStage<?>> stageFactory;
+  @Parameter(1)
+  public Function<Either<Throwable, ?>, CompletionStage<?>> completedStageFactory;
+
   @SuppressWarnings("unchecked")
+  private <T> CompletableStage<T> getCompletableStage() {
+    return (CompletableStage<T>) this.stageFactory.get();
+  }
+
+  @SuppressWarnings("unchecked")
+  private <T> CompletionStage<T> getCompletedStage(final T result) {
+    return (CompletionStage<T>) this.completedStageFactory.apply(Either.right(result));
+  }
+
+  @SuppressWarnings("unchecked")
+  private <T> CompletionStage<T> getExceptionalStage(final Throwable exc) {
+    return (CompletionStage<T>) this.completedStageFactory.apply(Either.left(exc));
+  }
+
+
+  @Test
   public void testAllOf() {
     final List<Integer> results = Arrays.asList(1, 2, 3);
     final List<CompletionStage<Integer>> collect =
-        results.stream().map(StageSupport::completedStage).collect(Collectors.toList());
+        results.stream().map(this::getCompletedStage).collect(Collectors.toList());
 
-    Combinators.allOf(collect).toCompletableFuture().join();
+    TestUtil.join(Combinators.allOf(collect));
     Assert.assertArrayEquals(results.toArray(),
-        Combinators.collect(collect).toCompletableFuture().join().toArray());
+        TestUtil.join(Combinators.collect(collect)).toArray());
     Assert.assertArrayEquals(results.toArray(),
-        Combinators.collect(collect, Collectors.toList()).toCompletableFuture().join().toArray());
+        TestUtil.join(Combinators.collect(collect, Collectors.toList())).toArray());
   }
 
   @Test
   public void testAllEmptyInput()
-      throws InterruptedException, ExecutionException, TimeoutException {
+      throws TimeoutException {
     {
-      final Collection<Object> collection = Combinators.collect(Collections.emptyList())
-          .toCompletableFuture().get(50, TimeUnit.MILLISECONDS);
+      final Collection<Object> collection =
+          TestUtil.join(Combinators.collect(Collections.emptyList()), 50, TimeUnit.MILLISECONDS);
       Assert.assertTrue(collection.isEmpty());
     }
 
     {
       final Collection<Object> collection =
-          Combinators.collect(Collections.emptyList(), Collectors.toList()).toCompletableFuture()
-              .get(50, TimeUnit.MILLISECONDS);
+          TestUtil.join(Combinators.collect(Collections.emptyList(), Collectors.toList()),
+              50, TimeUnit.MILLISECONDS);
       Assert.assertTrue(collection.isEmpty());
     }
 
     {
-      Combinators.allOf(Collections.emptyList()).toCompletableFuture().join();
+      TestUtil.join(Combinators.allOf(Collections.emptyList()));
     }
   }
 
@@ -72,36 +106,34 @@ public class CombinatorsTest {
         IntStream.range(0, NUM_FUTURES).boxed().collect(Collectors.toList());
     final List<CompletionStage<Integer>> collect = IntStream
         .range(0, NUM_FUTURES)
-        .mapToObj(StageSupport::completedStage)
+        .mapToObj(this::getCompletedStage)
         .collect(Collectors.toList());
 
-    Combinators.allOf(collect).toCompletableFuture().join();
+    TestUtil.join(Combinators.allOf(collect));
     Assert.assertArrayEquals(results.toArray(),
-        Combinators.collect(collect).toCompletableFuture().join().toArray());
+        TestUtil.join(Combinators.collect(collect)).toArray());
     Assert.assertArrayEquals(results.toArray(),
-        Combinators.collect(collect, Collectors.toList()).toCompletableFuture().join().toArray());
+        TestUtil.join(Combinators.collect(collect, Collectors.toList())).toArray());
   }
 
   @Test
-  @SuppressWarnings("unchecked")
   public void testAllOfError() {
-    final List<CompletableFuture<Integer>> futures =
+    final List<CompletionStage<Integer>> futures =
         Arrays.asList(
-            StageSupport.completedStage(1).toCompletableFuture(),
-            StageSupport.<Integer>exceptionalStage(new TestException()).toCompletableFuture());
+            getCompletedStage(1),
+            getExceptionalStage(new TestException()));
     assertError(Combinators.allOf(futures));
     assertError(Combinators.collect(futures));
     assertError(Combinators.collect(futures, Collectors.toList()));
   }
 
   @Test
-  @SuppressWarnings("unchecked")
   public void testAllOfErrorNoShortCircuit() {
-    final CompletableFuture<Integer> delayed = new CompletableFuture<>();
-    final List<CompletableFuture<Integer>> futures =
+    final CompletableStage<Integer> delayed = getCompletableStage();
+    final List<CompletionStage<Integer>> futures =
         Arrays.asList(
             delayed,
-            StageSupport.<Integer>exceptionalStage(new TestException()).toCompletableFuture());
+            getExceptionalStage(new TestException()));
 
     final CompletionStage<Void> voidAll = Combinators.allOf(futures);
     final CompletionStage<Collection<Integer>> collAll = Combinators.collect(futures);
@@ -124,9 +156,9 @@ public class CombinatorsTest {
     final Map<Integer, CompletionStage<Integer>> stageMap =
         IntStream.range(0, 5)
             .boxed()
-            .collect(Collectors.toMap(Function.identity(), StageSupport::completedStage));
+            .collect(Collectors.toMap(Function.identity(), this::getCompletedStage));
     final Map<Integer, Integer> integerMap =
-        Combinators.keyedAll(stageMap).toCompletableFuture().join();
+        TestUtil.join(Combinators.keyedAll(stageMap));
     Assert.assertEquals(5, integerMap.size());
     Assert.assertTrue(
         integerMap.entrySet().stream().allMatch(entry -> entry.getKey().equals(entry.getValue())));
@@ -139,22 +171,22 @@ public class CombinatorsTest {
             .boxed()
             .collect(Collectors.toMap(Function.identity(), i -> {
               if (i == 3) {
-                return StageSupport.exceptionalStage(new TestException());
+                return getExceptionalStage(new TestException());
               }
-              return StageSupport.completedStage(i);
+              return getCompletedStage(i);
             }));
     assertError(Combinators.keyedAll(stageMap));
   }
 
   @Test
   public void testKeyedAllErrorNoShortCircuit() {
-    final Map<Integer, CompletableFuture<Integer>> stageMap =
+    final Map<Integer, CompletableStage<Integer>> stageMap =
         IntStream.range(0, 5)
             .boxed()
-            .collect(Collectors.toMap(Function.identity(), i -> new CompletableFuture<>()));
+            .collect(Collectors.toMap(Function.identity(), i -> getCompletableStage()));
     final CompletionStage<Map<Integer, Integer>> fut = Combinators.keyedAll(stageMap);
     int i = 0;
-    for (final CompletableFuture<Integer> future : stageMap.values()) {
+    for (final CompletableStage<Integer> future : stageMap.values()) {
       assertIncomplete(fut);
       if (i == 3) {
         future.completeExceptionally(new TestException());
@@ -168,7 +200,7 @@ public class CombinatorsTest {
 
   private <T> void assertError(final CompletionStage<T> stage) {
     try {
-      stage.toCompletableFuture().join();
+      TestUtil.join(stage);
     } catch (final CompletionException e) {
       Assert.assertTrue(e.getCause() instanceof TestException);
     }
@@ -176,11 +208,8 @@ public class CombinatorsTest {
 
   private <T> void assertIncomplete(final CompletionStage<T> stage) {
     try {
-      stage.toCompletableFuture().get(20, TimeUnit.MILLISECONDS);
+      TestUtil.join(stage, 20, TimeUnit.MILLISECONDS);
       Assert.fail("not all futures complete, get should timeout");
-    } catch (final InterruptedException e) {
-    } catch (final ExecutionException e) {
-      Assert.fail(e.getMessage());
     } catch (final TimeoutException e) {
       // expected
     }
